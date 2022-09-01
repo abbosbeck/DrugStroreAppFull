081f01ba5a57559d776e034d469d0e9f5b9d54a5
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-09-01 16:00:08 +0500

    added DataAccess, attriburtes and some codes

--- Models/EmployeeModel.cs
+++ Models/EmployeeModel.cs
@@ -1,12 +1,20 @@
-﻿namespace DrugStroreAppFull.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrugStroreAppFull.Models
 {
     public class EmployeeModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Experinece { get; set; }
+        [Required]
         public string Region { get; set; }
+        [Required]
         public string Sex { get; set; }
     }
 }
